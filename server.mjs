03652bd2d92f44d1d2a74a2176e0a8c66e9950e2
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2022-11-06 19:14:14 +0500

    ready for cloud

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/name', (req, res) => {
+  console.log(`${req.ip}is asking for water`)
+  res.send('nadeem')
+})
+
+app.get('/water', (req, res) => {
+    res.send('here is your water')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
